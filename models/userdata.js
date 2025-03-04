f97489d4025d2b5ecc5b137835a1d2f15504bfde
Soni <soniagrawal0010@gmail.com>
2025-03-04 21:02:03 +0530

    final-update

--- models/userdata.js
+++ models/userdata.js
@@ -2,33 +2,27 @@ const mongoose = require("mongoose");
 
 const linkSchema = new mongoose.Schema({
     id: {
-        type: Number,
-        required: false,
-        unique: true
+        type: String,
+        unique: true,
+        sparse: true // Allows multiple null values
     },
     name: {
         type: String,
-        default: "",
-        required: false,
-        unique: true
+        required: false
     },
     url: {
         type: String,
-        default: "",
-        required: false,
-        unique: true
+        required: false
     },
-    icon : {
-        type : String,
-        default : "",
-        required : false
+    icon: {
+        type: String,
+        required: false
     },
-    iconColor : {
-        type : String, 
-        default : "",
-        required : false
+    iconColor: {
+        type: String,
+        required: false
     }
-})
+});
 
 const shopSchema = new mongoose.Schema({
     id: {
@@ -36,17 +30,15 @@ const shopSchema = new mongoose.Schema({
         required: false,
         unique: true
     },
-    name : {
+    name: {
         type: String,
-        default: "",
         required: false
     },
     url: {
         type: String,
-        default: "",
         required: false
     }
-})
+});
 
 const userSchema = new mongoose.Schema({
     firstname: {
@@ -55,60 +47,59 @@ const userSchema = new mongoose.Schema({
     },
     lastname: {
         type: String,
-        required:false,
+        required: false
     },
     email: {
         type: String,
         unique: true,
-        required : true
+        required: true
     },
     password: {
         type: String,
         required: true
     },
-    category:{
-        type:String,
-        required:false
+    category: {
+        type: String,
+        required: false
     },
-    profile:{
-        type:String,
-        default:"https://res.cloudinary.com/dtqlvrklv/image/upload/v1740577262/6f09aa6ed839fa7b57767edf7998c27a_gjnz7z.png",
-        required:false
+    profile: {
+        type: String,
+        default: "https://res.cloudinary.com/dtqlvrklv/image/upload/v1740577262/6f09aa6ed839fa7b57767edf7998c27a_gjnz7z.png",
+        required: false
     },
-    username : {
-        type: String, 
-        default: "",
-        required: false,
-        unique: true
-    },
-    buttonType : {
+    username: {
         type: String,
         default: "",
         required: false
     },
-    layoutType : {
+    buttonType: {
         type: String,
         default: "",
         required: false
     },
-    buttonColor : {
+    layoutType: {
         type: String,
         default: "",
         required: false
     },
-    buttonFontColor : {
+    buttonColor: {
         type: String,
         default: "",
         required: false
     },
-    font : {
+    buttonFontColor: {
         type: String,
         default: "",
         required: false
     },
-    fontColor : {
-        type : String,
-        default : "",
+    font: {
+        type: String,
+        default: "",
+        required: false
+    },
+    fontColor: {
+        type: String,
+        default: "",
         required: false
     },
     theme: {
@@ -121,13 +112,13 @@ const userSchema = new mongoose.Schema({
         default: "#342B26",
         required: false
     },
-    socialLinks: [linkSchema],
+    socialLinks: [linkSchema], // Ensuring this does not cause duplicate errors
     shopLinks: [shopSchema],
-    bio : {
-        type : String,
-        default : "",
-        required : false
+    bio: {
+        type: String,
+        default: "",
+        required: false
     }
-})
+});
 
-module.exports=mongoose.model("users", userSchema)
+module.exports = mongoose.model("users", userSchema);
